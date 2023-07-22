cb45d5735d0f573cb34e85470450838ed0fd9523
anoying-kid <yugrana853@gmail.com>
2023-07-22 12:43:03 +0530

    Done basic files Place class, GreatPlaces provider and the screens

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.add))
+        ],
+      ),
+      body: Center(child: CircularProgressIndicator.adaptive()),
+    );
+  }
+}
